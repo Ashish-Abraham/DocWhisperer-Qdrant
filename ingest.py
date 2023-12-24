bcb9994422248572483080bcb9cd6cc80e7bc033
Ashish Abraham <83216788+Ashish-Abraham@users.noreply.github.com>
2023-12-24 13:24:45 +0530

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,25 +1,56 @@
-from langchain.document_loaders import PyPDFLoader, PDFMinerLoader, DirectoryLoader
-from langchain.embeddings import SentenceTransformerEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
 import os
-from constants import CHROMA_SETTINGS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import PyPDFLoader
+from qdrant_client import QdrantClient
+from qdrant_client.http import models
+import torch
+from sentence_transformers import SentenceTransformer
 
-def main():
-    for root,dir,files in os.walk("docs"):
-        for file in files:
-            if file.endswith(".pdf"):
-                loader = PDFMinerLoader(path=os.join(root,file))
+model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
 
-    documents = loader.load()
-    textsplitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=500)
-    texts = textsplitter.split_documents(documents)
 
-    embeddings = SentenceTransformerEmbeddings(model_name="")
 
-    db = Chroma.from_documents(texts, embeddings, persist_directory="db", client_settings=CHROMA_SETTINGS)
-    db.persist()
-    db=None
+loader = PyPDFLoader("docs/ASM-standards.pdf")
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=700, chunk_overlap=50)
+texts = text_splitter.split_documents(documents)
 
-if __name__=="__main__":
-    main()
+qdrant_client = QdrantClient(host='localhost', port=6333)
+my_collection = "ASM"
+qdrant_client.recreate_collection(
+    collection_name=my_collection,
+    vectors_config=models.VectorParams(size=384, distance=models.Distance.COSINE)
+)
+
+
+
+# Generate embeddings for each text chunk
+embeddings_list = []
+points_list = []
+i=0
+for text in texts:
+    embedding = model.encode(text.page_content)
+    point_dict = {
+        "id": i+1,
+        "vector": embedding,
+        "payload": {"text": text.page_content},
+    }
+    points_list.append(point_dict)
+    i+=1
+    embeddings_list.append(embedding)
+
+
+
+print("Embeddings Successful")    
+
+
+
+
+
+qdrant_client.upsert(collection_name=my_collection, points=points_list)
+
+
+
+
+print("Vector DB Successfully Created!")
